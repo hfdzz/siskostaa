42b50ca4451497dc1523f06321de21052b287a17
Satriomj <satriosianturi17@gmail.com>
2023-10-29 01:03:43 +0700

    up dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
 // artisan command line interface in laravel
