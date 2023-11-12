dec1308104403bcb0d8672a4e8d33477b6138ec2
120140219 <rafiirfannn@gmail.com>
2023-11-12 17:03:35 +0700

    up admin-pembayaran, modif admin-pesanan

--- resources/views/admin/validasi-pesanan.blade.php
+++ resources/views/admin/validasi-pesanan.blade.php
@@ -18,7 +18,7 @@
             <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom">
                 Kost Abang Adek</div>
             <div class="list-group list-group-flush my-3">
-                <a href="/admin" class="list-group-item list-group-item-action bg-transparent second-text active"><i
+                <a href="/admin" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
                         class="fas fa-solid fa-house me-2"></i>Dashboard</a>
                 <a href="/admin-profilKost" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
                         class="fas fa-solid fa-list me-2"></i>Profile Kost</a>
