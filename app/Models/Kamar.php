07ab921cf9fc294107282cf0edc014af1f35c0c9
hfdzz <yhafidz@gmail.com>
2023-11-19 19:03:09 +0700

    kamar add occupant method

--- app/Models/Kamar.php
+++ app/Models/Kamar.php
@@ -27,4 +27,20 @@ class Kamar extends Model
     {
         return $this->nomor_kamar . $this->kode_gedung;
     }
+
+    public function addOcupant($pemesanan_id)
+    {
+        $this->update([
+            'pemesanan_id' => $pemesanan_id,
+            'status_available' => 0,
+        ]);
+    }
+
+    public function removeOcupant()
+    {
+        $this->update([
+            'pemesanan_id' => null,
+            'status_available' => 1,
+        ]);
+    }
 }
