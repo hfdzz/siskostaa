d9f36b99ee64f928001870c39f656eff2b08587a
hfdzz <yhafidz@gmail.com>
2023-12-21 09:52:04 +0700

    filesystem config public disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,9 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            // 'root' => storage_path('app/public'),
+            // root to public_html/storage
+            'root' => public_path() . '/storage',
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
